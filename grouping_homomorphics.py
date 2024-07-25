47d41ba0366ca852e78178ca5b746cee3e63d382
AcengAkii <2432329@students.wits.ac.za>
2024-07-25 10:33:35 +0200

    Update grouping_homomorphics
    Updated the function to return the edge sequences for each matrix as keeys.

--- grouping_homomorphics.py
+++ grouping_homomorphics.py
@@ -2,15 +2,18 @@
 # homomorphic to one another. 
 
 import numpy as np
+import drawing as dr
 from collections import defaultdict
 
-#A counting function - edge sequences
+#A counting function - to determine the edge sequences
 def ones_per_row(matrix): #Another way to find the 'degree' of a vertex
     return [row.count(1) for row in matrix]
 
 #Function takes in a list of matrices---
 def group_matrices_by_ones(matrices): #regardless of row order group the matrices, according to ones per row.
     groups = defaultdict(list) 
+    llist = []
+    keeys = []
     
     for matrix in matrices:
         ones_count = ones_per_row(matrix)
@@ -18,19 +21,28 @@ def group_matrices_by_ones(matrices): #regardless of row order group the matrice
         groups[ones_count_sorted].append(matrix)
 
     for key, group in groups.items():
+        A = np.array(group)
+        ####################################################
+        #uncomment this for a pretty view
         print(f"Group {key}:")
         print(f"representative matrix")
         print()
-        A = np.array(group)
         print(A[0])
 
         #To draw the graph
-        #B = draw_graph(A[0])
-        #print(B)
+        B = dr.draw_graph(A[0])
         print("=" * 40)
+        #####################################################
+        
+        llist.append(A[0].tolist())
+        keeys.append(key)
         
     nnn = len(groups)
-    rep_els = {key: group[0] for key, group in groups.items()}
     
-    return rep_els, nnn
+    print(f'We can expect {nnn} groups of graphs.')
+    print()
+    
+        
+    return llist, keeys
+#where the keeys are the edge sequences.
 
