000d7d288f81e0fefe16374b709c32851d6412dd
AcengAkii <2432329@students.wits.ac.za>
2024-09-16 17:04:56 +0200

    update adjacency tests

--- function_drawing.py
+++ function_drawing.py
@@ -11,45 +11,46 @@ import math
 #working with a list of them.
 
 def draw_graph(adj_matrix):
-    # Create an empty graph
-    n = len(adj_matrix)
-    G = nx.Graph()
-    # Add nodes
-    G.add_nodes_from(range(n))
-    # Add edges
-    for i in range(n):
-        for j in range(i+1, n):
-            if adj_matrix[i][j] == 1:
-                G.add_edge(i, j, color= 'blue')
-            else:
-                G.add_edge(i,j, color = 'red')
-                
-    # Print the graph
-    print("Graph:")
-    print(G.edges())
+	# Create an empty graph
+	n = len(adj_matrix)
+	G = nx.Graph()
+
+	# Add nodes
+	G.add_nodes_from(range(n))
+
+	# Add edges
+	for i in range(n):
+		for j in range(i+1, n):
+			if adj_matrix[i][j] == 1:
+				G.add_edge(i, j, color= 'blue')
+			else:
+				G.add_edge(i,j, color = 'red')
+
+	# Print the graph
+	print("Graph:")
+	print(G.edges())
     
-    pos = {}
-    for i in range(n):
-        angle = 2 * math.pi * i / n
-        pos[i] = (math.cos(angle) + 1) / 2, (math.sin(angle) + 1) / 2
+	pos = {}
+	for i in range(n):
+		angle = 2 * math.pi * i / n
+		pos[i] = (math.cos(angle) + 1) / 2, (math.sin(angle) + 1) / 2
         
-                
-    edges = G.edges()
-    colors = [G[u][v]['color'] for u, v in edges]
+	edges = G.edges()
+	colors = [G[u][v]['color'] for u, v in edges]
     
-    # Visualize the graph
-    
-    nx.draw(G, pos , edge_color = colors , width = 3, with_labels=True)
+	# Visualize the graph
 
-    plt.title("Graph Visualization")
+	nx.draw(G, pos , edge_color = colors , width = 3, with_labels=True)
 
-    #plt.savefig(f'graph1111_{adj_matrix}')
-    plt.show()
-    plt.clf()
-            
-    print()
-    
-    return 
+	plt.title("Graph Visualization")
+
+	#plt.savefig(f'graph1111_{adj_matrix}')
+	plt.show()
+	plt.clf()
+	    
+	print()
+
+	return 
 
 
 def draw_graph_1color(adj_matrix):
--- test_adjacency.py
+++ test_adjacency.py
@@ -5,21 +5,22 @@ import function_grouping_homomorphics as gh
 import function_drawing as dr
 
 # test
-n = 2
+n = 4
 adjacency_matrices = aj.all_adj_matrix(n)
 
 for i, matrix in enumerate(adjacency_matrices):
 	print(f"Matrix {i+1}:")
 	print(matrix)
-	print()   
+	print("\n")   
 
 A = gh.group_matrices_by_ones(adjacency_matrices)
 print(A)
+print("\n")   
 
 
-for matrix in A:
+for matrix in A[0]:
+	print(matrix)
+	print(len(matrix))
+	print("\n")   
 	dr.draw_graph(matrix)
-    
-    
-
 
